9a13f9d1ed1b10010a03ffb72a1462f0e243d410
SomRav <soumyachakra004@gmail.com>
2025-03-30 20:10:51 +0530

    create dist

--- backend/index.js
+++ backend/index.js
@@ -52,6 +52,7 @@ let notes = [
 ];
 
 app.use(express.json());
+app.use(express.static("dist"));
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -105,7 +106,7 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
